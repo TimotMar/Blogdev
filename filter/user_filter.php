1d1a6fe57257f72612df8736e6c7a1efc46d14c0
TimotMar <35291868+TimotMar@users.noreply.github.com>
2018-03-05 14:22:49 +0100

    Add files via upload

---
+++ filter/user_filter.php
@@ -0,0 +1,12 @@
+<?php
+
+
+if(!isset($_SESSION['user_id']) && !isset($_SESSION['pseudo'])){
+$_SESSION['forwarding_url'] = $_SERVER['REQUEST_URI'];
+//on a sauvegardé en session l'URL vers lequel l'utilisation voulait se diriger
+    $_SESSION['notification']['message'] = 'Vous devez être connecté pour accéder à cette page';
+    $_SESSION['notification']['type'] = 'danger';
+    header('Location: login.php'); // redirection
+    exit();
+}
+?>
